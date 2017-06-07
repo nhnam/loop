165e8718051acdf341858e1d86e08b90a372b510
Tan Vu <tanvu@SmartDevs-Mac-mini.local>
2017-06-07 17:29:37 +0700

    Init project

---
+++ Loop/ViewControllers/Events/EventsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EventsViewController.swift
+//  Loop
+//
+//  Created by Tan Vu on 6/7/17.
+//  Copyright © 2017 Tan Vu. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+}
---
+++ Loop/ViewControllers/Home/MainTabsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTabsViewController.swift
+//  Loop
+//
+//  Created by Tan Vu on 6/7/17.
+//  Copyright © 2017 Tan Vu. All rights reserved.
+//
+
+import UIKit
+
+class MainTabsViewController: UIViewController {
+}
